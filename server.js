4b6b21cabfae85890ff9120802b33750f861600e
vanditha <vankadarivanditha6211@gmail.com>
2024-06-17 20:04:14 +0530

    category changes

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-import express from 'express'
-import dotenv from 'dotenv'
+import express from 'express';
+import dotenv from 'dotenv';
 import morgan from 'morgan';
 import connectDB from './config/db.js';
-import authRoutes from './routes/authRoute.js'
-import cors from 'cors'
-import safeBuffer from 'safe-buffer'
+import authRoutes from './routes/authRoute.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from './routes/productRoutes.js';
+import cors from 'cors';
+import safeBuffer from 'safe-buffer';
 //configuring env
 dotenv.config()
 
@@ -15,16 +17,17 @@ connectDB();
 const app = express();
 
 //middlewares
-app.use(express.json())
+app.use(express.json());
 app.use(morgan('dev'));
 
 
 //enable CORS
-
 app.use(cors());
 
 //routes
-app.use('/',authRoutes)
+app.use('/',authRoutes);
+app.use('/',categoryRoutes);
+app.use('/',productRoutes);
 
 //rest api
 app.get('/',(req,res)=>{
@@ -40,5 +43,5 @@ const PORT = process.env.PORT || 8080
 //run listen
 
 app.listen(PORT,()=>{
-    console.log(`server running on ${PORT}`)  
-})
+    console.log(`server running on ${PORT}`);
+});
