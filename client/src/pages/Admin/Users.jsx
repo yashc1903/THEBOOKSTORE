64d544008d91a83d471f596b8d51085f1588c34d
vanditha6211 <131430621+vanditha6211@users.noreply.github.com>
2024-07-01 17:32:30 +0530

    Update Users

--- client/src/pages/Admin/Users.jsx
+++ client/src/pages/Admin/Users.jsx
@@ -1,25 +1,20 @@
-import React from 'react'
-import Layout from '../../components/Layout'
-import AdminMenu from '../../components/AdminMenu'
+import React from "react";
+import Layout from "../../components/Layout";
+import AdminMenu from "../../components/AdminMenu";
 
 function Users() {
   return (
-
-    <>
-        <Layout>
-        <div className='flex justify-around items-center'>
-        <div>
-
-      <AdminMenu/>
+    <Layout>
+      <div className="flex flex-col lg:flex-row justify-center min-h-screen items-start lg:items-stretch">
+        <div className="w-full lg:w-1/4 p-4 lg:p-6">
+          <AdminMenu />
         </div>
-        <div className='border w-1/2 h-96 '>
-          <h1 className='text-3xl w-full mt-10'>All Users</h1>
-         
+        <div className="w-full lg:w-3/4 p-4 lg:p-6 border rounded-md shadow-lg">
+          <h1 className="text-3xl text-center mb-6">All Users</h1>
         </div>
       </div>
-        </Layout>
-    </>
-  )
+    </Layout>
+  );
 }
 
-export default Users
+export default Users;
